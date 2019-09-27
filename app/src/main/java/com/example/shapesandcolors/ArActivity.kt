92df6155dde5596ce997301e4a0c640134e9bdf8
HyeSoo <flyjwayur@gmail.com>
2019-09-27 14:59:50 +0300

    Fix the ar activity with correct layout by testing

--- app/src/main/java/com/example/shapesandcolors/ArActivity.kt
+++ app/src/main/java/com/example/shapesandcolors/ArActivity.kt
@@ -1,25 +1,15 @@
 package com.example.shapesandcolors
 
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 
 class ArActivity : AppCompatActivity() {
 
     private lateinit var fragment: ArFragment
-    private lateinit var modelUri: Uri
-    private lateinit var fitToScanImageView : ImageView
-    private var circleRenderable: ModelRenderable? = null
-    private var heartRenderable: ModelRenderable? = null
-    private var hexagonRenderable: ModelRenderable? = null
-    private var selectedShape:Int = 1
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        fragment = supportFragmentManager.findFragmentById(R.id.arimage_fragment) as ArFragment
+        setContentView(R.layout.activity_ar)
+        fragment = supportFragmentManager.findFragmentById(R.id.arimage_fragment) as ArFragment
     }
 }
