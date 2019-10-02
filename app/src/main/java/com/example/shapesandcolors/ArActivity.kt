ecf8fecbde2b9dae740df5f43874979912ab963b
gbenarh <gbenarh@gmail.com>
2019-10-02 21:02:47 +0300

    Add media Input Activity and it's UI, Cancel button working but not perfect

--- app/src/main/java/com/example/shapesandcolors/ArActivity.kt
+++ app/src/main/java/com/example/shapesandcolors/ArActivity.kt
@@ -1,21 +1,18 @@
 package com.example.shapesandcolors
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.net.Uri
 import android.widget.TextView
-import android.widget.Toast
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.TransformableNode
 import com.google.ar.sceneform.ux.ArFragment
 import kotlinx.android.synthetic.main.activity_ar.*
 import com.google.ar.sceneform.math.Vector3
-import com.google.ar.sceneform.math.Quaternion
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
 import com.google.ar.sceneform.rendering.ViewRenderable
 
 
@@ -41,12 +38,17 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ar)
 
+        if (savedInstanceState != null) {
+            fragment = getSupportFragmentManager().getFragment(savedInstanceState, "arFragment") as ArFragment
+        }
+
         arrayView = arrayOf(
             imv_circle, imv_heart, imv_hexagon, imv_circle2, imv_heart2,
             imv_hexagon2, imv_circle3, imv_heart3, imv_hexagon3, imv_circle4)
 
         setUpClickListener()
         setUpModel()
+
 
         fragment = supportFragmentManager.findFragmentById(R.id.arimage_fragment) as ArFragment
         fragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
@@ -57,6 +59,12 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
             createModel(anchorNode, selected)
         }
 
+    }
+
+    override fun onSaveInstanceState(outState : Bundle) {
+        super.onSaveInstanceState(outState)
+        //Save the fragment's instance
+        getSupportFragmentManager().putFragment(outState, "arFragment", fragment)
     }
 
     private fun setUpClickListener() {
@@ -174,7 +182,7 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
                 circle.renderable = circleRenderable
                 circle.select()
 
-                addShapeLabel(anchorNode, circle, "Tap to name")
+                addShapeLabel(anchorNode, circle, "Tap to name", selected)
             }
             2 -> {
                 val diamond = TransformableNode(fragment.transformationSystem)
@@ -184,7 +192,7 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
                 diamond.renderable = diamondRenderable
                 diamond.select()
 
-                addShapeLabel(anchorNode, diamond, "Tap to name")
+                addShapeLabel(anchorNode, diamond, "Tap to name", selected)
             }
             3 -> {
                 val heart = TransformableNode(fragment.transformationSystem)
@@ -194,7 +202,7 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
                 heart.renderable = heartRenderable
                 heart.select()
 
-                addShapeLabel(anchorNode, heart, "Tap to name")
+                addShapeLabel(anchorNode, heart, "Tap to name", selected)
             }
             4 -> {
                 val hexagon = TransformableNode(fragment.transformationSystem)
@@ -204,7 +212,7 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
                 hexagon.renderable = hexagonRenderable
                 hexagon.select()
 
-                addShapeLabel(anchorNode, hexagon, "Tap to name")
+                addShapeLabel(anchorNode, hexagon, "Tap to name", selected)
             }
             5 -> {
                 val octagon = TransformableNode(fragment.transformationSystem)
@@ -214,7 +222,7 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
                 octagon.renderable = octagonRenderable
                 octagon.select()
 
-                addShapeLabel(anchorNode, octagon, "Tap to name")
+                addShapeLabel(anchorNode, octagon, "Tap to name", selected)
             }
             6 -> {
                 val oval = TransformableNode(fragment.transformationSystem)
@@ -224,7 +232,7 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
                 oval.renderable = ovalRenderable
                 oval.select()
 
-                addShapeLabel(anchorNode, oval, "Tap to name")
+                addShapeLabel(anchorNode, oval, "Tap to name", selected)
             }
             7 -> {
                 val rectangle = TransformableNode(fragment.transformationSystem)
@@ -234,7 +242,7 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
                 rectangle.renderable = rectangleRenderable
                 rectangle.select()
 
-                addShapeLabel(anchorNode, rectangle, "Tap to name")
+                addShapeLabel(anchorNode, rectangle, "Tap to name", selected)
             }
             8 -> {
                 val square = TransformableNode(fragment.transformationSystem)
@@ -244,7 +252,7 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
                 square.renderable = squareRenderable
                 square.select()
 
-                addShapeLabel(anchorNode, square, "Tap to name")
+                addShapeLabel(anchorNode, square, "Tap to name", selected)
             }
             9 -> {
                 val star = TransformableNode(fragment.transformationSystem)
@@ -254,7 +262,7 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
                 star.renderable = starRenderable
                 star.select()
 
-                addShapeLabel(anchorNode, star, "Tap to name")
+                addShapeLabel(anchorNode, star, "Tap to name", selected)
             }
             10 -> {
                 val triangle = TransformableNode(fragment.transformationSystem)
@@ -265,12 +273,12 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
                 triangle.renderable = triangleRenderable
                 triangle.select()
 
-                addShapeLabel(anchorNode, triangle, "Tap to name")
+                addShapeLabel(anchorNode, triangle, "Tap to name", selected)
             }
         }
     }
 
-    private fun addShapeLabel(anchorNode: AnchorNode, node: TransformableNode, label: String) {
+    private fun addShapeLabel(anchorNode: AnchorNode, node: TransformableNode, label: String, id : Int) {
         ViewRenderable.builder().setView(this, R.layout.label_layout)
             .build()
             .thenAccept{ viewRenderable ->
@@ -282,9 +290,13 @@ class ArActivity : AppCompatActivity(), View.OnClickListener {
 
                 val textLabel = viewRenderable.view as TextView
                 textLabel.text = label // set's the text according to the label parsed
+
+                // Launch fragment for media capture
                 textLabel.setOnClickListener{
-                    anchorNode.setParent(null)
+                    val intent = Intent(this, MediaInputActivity::class.java)
+                    startActivity(intent)
                 }
             }
     }
+
 }
---
+++ app/src/main/java/com/example/shapesandcolors/MediaFragment.kt
@@ -0,0 +1,55 @@
+package com.example.shapesandcolors
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_media_input.view.*
+
+private const val ARG1 = "param1"
+
+class MediaFragment: Fragment() {
+
+    private lateinit var viewOfFragment: View
+    private var param1: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if(arguments != null) {
+            param1 = arguments!!.getInt(ARG1)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle? ): View? {
+        viewOfFragment = inflater.inflate(R.layout.activity_media_input, container, false)
+
+        viewOfFragment.btn_save.setOnClickListener { onSaveMedia(it) }
+        viewOfFragment.btn_cancel.setOnClickListener { onCancelMedia(it) }
+        return  viewOfFragment
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: Int): MediaFragment {
+            val args: Bundle = Bundle()
+            args.putInt(ARG1,param1)
+            val fragment = MediaFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    private fun onSaveMedia(v: View?) {
+        // code here
+    }
+
+    private fun onCancelMedia(v: View?) {
+        // code here
+        // imv_scrollview.visibility = View.VISIBLE
+        getActivity()?.onBackPressed()
+    }
+}
