3414eaedc8a6d098ecbf777f071231c871e715ea
HyeSoo <flyjwayur@gmail.com>
2019-10-07 11:23:09 +0300

    Add color and test colors in API

--- app/src/main/java/com/example/shapesandcolors/model/GlobalModel.kt
+++ app/src/main/java/com/example/shapesandcolors/model/GlobalModel.kt
@@ -7,7 +7,13 @@ object GlobalModel {
     data class color(val name: String)
 
     init {
+        colors.add(ColorData("red", "#FF0000"))
+        colors.add(ColorData("blue", "#0000FF"))
+        colors.add(ColorData("yellow", "#FFFF00"))
+        colors.add(ColorData("orange", "#FFA500"))
+        colors.add(ColorData("green", "#00FF00"))
+        colors.add(ColorData("purple", "#AD26D8"))
         colors.add(ColorData("black", "#000000"))
-        colors.add(ColorData("green", "#008000"))
+        colors.add(ColorData("white", "#FFFFFF"))
     }
 }
