15e6b11ea98653119f897a4a3b509a417d665b82
HyeSoo <flyjwayur@gmail.com>
2019-10-08 12:22:09 +0300

    Check whether a selected color is matched with an image color

--- app/src/main/java/com/example/shapesandcolors/FetchImageActivity.kt
+++ app/src/main/java/com/example/shapesandcolors/FetchImageActivity.kt
@@ -15,6 +15,7 @@ import com.example.shapesandcolors.model.*
 import kotlinx.android.synthetic.main.activity_fetch_image.*
 import retrofit2.Call
 import retrofit2.Response
+import kotlin.collections.HashMap
 
 
 class FetchImageActivity : AppCompatActivity() {
@@ -22,47 +23,60 @@ class FetchImageActivity : AppCompatActivity() {
     private val queryService = QueryApiService()
     //Image URL from Color API to fetch color square
     private var imageLink: String? = null
-    val colorOjects: HashMap<String, Int> =
-        hashMapOf(
-            "Apple" to R.drawable.apple,
-            "Fish" to R.drawable.fish,
-            "Sun" to R.drawable.sun,
-            "Orange" to R.drawable.oranges,
-            "Watermelon" to R.drawable.watermelon,
-            "Eggplant" to R.drawable.eggplant,
-            "Tire" to R.drawable.tire,
-            "Snowman" to R.drawable.snowman
-        )
+    private var selectedHex: String? = null
+    val colorImageObjects: HashMap<String, ColorImageData> = HashMap()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fetch_image)
 
-        val adapter = ColorListAdapter(this, GlobalModel.colors)
-        listView_colors.adapter = adapter
-
+        //Display a color palette with a recycler + grid view
         recyclerV_colors_grid.layoutManager = GridLayoutManager(this, 3)
         recyclerV_colors_grid.adapter =
             ColorsAdapter(GlobalModel.colors, this, { hexItem -> fetchColorFromAPI(hexItem) })
 
-        val selectedColorObject = colorOjects.values.random()
-        val keyOfSelectColorObject =
-            colorOjects.filterValues { it == selectedColorObject }.keys.first()
-        Log.d("DBG Message", keyOfSelectColorObject)
-        imgV_colorObject.setImageResource(selectedColorObject)
-        textV_colorGameDesc2.text =
-            "Choose the color of $keyOfSelectColorObject from the color palette"
+        //Add values to the colorImageObjects hashmap
+        addValuesToColorImageObjects()
 
-        listView_colors.setOnItemLongClickListener { _, _, position, _ ->
-            val selectedColor = GlobalModel.colors[position]
-            val detailIntent = FetchImageDetailActivity.newIntent(this, selectedColor)
-            startActivity(detailIntent)
-            true
+        val keyOfselectedColorObject = colorImageObjects.keys.random()
+        val valueOfSelectedColorObject = colorImageObjects[keyOfselectedColorObject]
+        val colorOfselectedColorObject = valueOfSelectedColorObject!!.color
+        val srcOfselectedColorObject = valueOfSelectedColorObject!!.imgSrc
+
+        imgV_colorObject.setImageResource(srcOfselectedColorObject)
+        textV_colorGameDesc2.text =
+            "What is a color of $keyOfselectedColorObject ? "
+
+        button_check.setOnClickListener { it ->
+            var nameOfSelectedHex:String
+
+            if (selectedHex !== null) {
+                nameOfSelectedHex =
+                    GlobalModel.colors.filter { it -> it.hex === selectedHex }.first().name
+                Log.d("TEST", nameOfSelectedHex)
+                if (colorOfselectedColorObject === nameOfSelectedHex) {
+                    Log.d("TEST -true", "TRUE")
+                } else {
+                    Log.d("TEST - false", "FALSE")
+                }
+            }
         }
     }
 
+    private fun addValuesToColorImageObjects() {
+        colorImageObjects["Apple"] = ColorImageData("red", R.drawable.apple)
+        colorImageObjects["Fish"] = ColorImageData("blue", R.drawable.fish)
+        colorImageObjects["Sun"] = ColorImageData("yellow", R.drawable.sun)
+        colorImageObjects["Orange"] = ColorImageData("orange", R.drawable.oranges)
+        colorImageObjects["Watermelon"] = ColorImageData("green", R.drawable.watermelon)
+        colorImageObjects["Eggplant"] = ColorImageData("purple", R.drawable.eggplant)
+        colorImageObjects["Tire"] = ColorImageData("black", R.drawable.tire)
+        colorImageObjects["Snowman"] = ColorImageData("white", R.drawable.snowman)
+    }
+
     private fun fetchColorFromAPI(hex: String) {
+        selectedHex = hex
         val call: Call<QueryResult> = queryService.getQuery(hex)
         call.enqueue(object : retrofit2.Callback<QueryResult> {
             override fun onFailure(call: Call<QueryResult>, t: Throwable) {
@@ -75,40 +89,5 @@ class FetchImageActivity : AppCompatActivity() {
                 webV_APIshape.loadUrl(imageLink)
             }
         })
-    }
-
-
-    private inner class ColorListAdapter(
-        context: Context,
-        private val colors: MutableList<ColorData>
-    ) : BaseAdapter() {
-
-        private val inflater: LayoutInflater =
-            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-
-        override fun getCount(): Int {
-            return colors.size
-        }
-
-        override fun getItem(position: Int): Any {
-            return colors[position]
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-            val rowView = inflater.inflate(R.layout.item_color, parent, false)
-
-            val thisColorData = colors[position]
-            var tv = rowView.findViewById(R.id.tvName) as TextView
-            tv.text = thisColorData.name
-
-            tv = rowView.findViewById(R.id.tvHex) as TextView
-            tv.text = thisColorData.hex
-
-            return rowView
-        }
     }
 }
